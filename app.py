613b5736b46522328b8014cef8e6f0ebe2d8686c
Arjuna Anuhass <anuhass2017@gmail.com>
2026-09-25 14:16:41 +0530

    feat: pipeline of vector store

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from src.data_loader import load_all_documents
-
+from src.vectorstore import FaissVectorStore
 ## Example usage
 
 if __name__ == '__main__':
     docs=load_all_documents("data")
-
-    print(docs)
+    store=FaissVectorStore("faiss_store")
+    # store.build_from_documents(docs) 
+    store.load()
+    print(store.query("What is Arjuna Anuhass Resume?", top_k=3))
